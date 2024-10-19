199cf1e5f1051740ba87d3343decd49fc3773075
The9darknight <the9darknight@gmail.com>
2024-10-19 13:59:05 -0500

    starting on the chatrooms
    chatrooms and users are added

---
+++ db/migrate/20241019185152_create_chatrooms.rb
@@ -0,0 +1,10 @@
+class CreateChatrooms < ActiveRecord::Migration[7.2]
+  def change
+    create_table :chatrooms do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
