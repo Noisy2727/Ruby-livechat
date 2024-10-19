d74539a5caef40a622fe3f9a8c31d2d30d52552f
The9darknight <the9darknight@gmail.com>
2024-10-19 15:01:35 -0500

    Very basic chatrooms

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,27 @@
 class ChatroomsController < ApplicationController
   def index
+    @chatrooms = Chatroom.all
+  end
+
+  def show
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+
+    if @chatroom.save
+      redirect_to @chatroom
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def chatroom_params
+    params.require(:chatroom).permit(:title, :body)
   end
 end
