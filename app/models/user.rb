618247a6bf8a07c5d3d1b5119a684e3a8f57eaa9
The9darknight <the9darknight@gmail.com>
2024-10-19 19:56:08 -0500

    Delete user.rb

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  validate_uniqueness_of :username # names witll be unique
-  scope :all_except, ->(user) { where.not(id: user) } # users will not be able to see themselelves
-end
