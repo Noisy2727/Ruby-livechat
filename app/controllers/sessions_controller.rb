28b6e532f9b7d6bb5c7bc528b9cbc76dc902a37e
The9darknight <the9darknight@gmail.com>
2024-11-13 12:12:38 -0600

    Users created

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      #render json: user, status: :ok
+      redirect_to root_path, notice: 'User created logged in!'
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    head :no_content
+  end
+end
