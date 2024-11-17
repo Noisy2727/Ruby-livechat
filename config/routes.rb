c6d8fa374da9c356dea22e00edace8b9103c38f5
The9darknight <the9darknight@gmail.com>
2024-11-16 22:57:07 -0600

    Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,24 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :chatrooms
   resources :users
-  root 'chatrooms#index'
+  #root 'chatrooms#index'
   get "chatrooms/index"
   mount ActionCable.server => "/chatroom"
 
   #root "chatrooms#index"
   # root "users#index"
-=======
   root "pages#home"
-
->>>>>>> Chats
   resources :chatrooms do
     resources :messages
   end
-<<<<<<< HEAD
   #get "/chatrooms", to: "chatrooms#index"
-=======
 
   get "/chatrooms", to: "chatrooms#index"
->>>>>>> Chats
   get "/users", to: "users#index"
   get "/user", to: "users#show"
 
-<<<<<<< HEAD
   resources :users, only: [ :new, :create ]
-=======
   resources :users
->>>>>>> Chats
   get "/login", to: "sessions#new"  # For displaying the login form
   post "/login", to: "sessions#create"  # For handling the login submission
   resources :sessions
